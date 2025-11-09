5f2d8e708feee8586905e45b3279bfb12c6664ae
kenkaroki <kenkaroki92@gmail.com>
2025-11-09 23:40:37 +0300

    Deployed by kenkaroki at 20:40:30 GMT, latitude -1.2833, longitude 36.8167, location Nairobi, KE

---
+++ _app/immutable/entry/start.C3yrQKgR.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/uUA3YQhn.js";export{o as load_css,r as start};
---
+++ _app/immutable/nodes/5.BlrW8A2v.js
@@ -0,0 +1,1 @@
+import"../chunks/BXefXNV-.js";import"../chunks/CYpwjcZg.js";import{S as p}from"../chunks/8lkf4rqD.js";function i(o){p(o,{})}export{i as component};
